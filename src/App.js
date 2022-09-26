a0b77bd24a443a22daa2d1514d87583deec21ae5
Moin <khaja0526@gmail.com>
2022-09-26 19:09:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Moin</h1>
+      <h1>Hello ismail</h1>
       <h2> magic happen!</h2>
     </div>
   );
